85f7d6586b1a0a5ab77a8f96d988fc8d07f3e268
olddunk <yegc22@gmail.com>
2022-06-08 15:51:44 +0800

    chapter2.14 Named Routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,31 @@
 <template>
-    <div class="home">
-        <h1>All Destinations</h1>
-        <div class="destinations">
-            <router-link v-for="destination in destinations" :to="destination.slug" :key="destination.id">
-                <h2>{{destination.name}}</h2>
-                <img :src="`/images/${destination.image}`" alt="destination.name">
-            </router-link>
-        </div>
+  <div class="home">
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <router-link
+        v-for="destination in destinations"
+        :to="{
+          name: 'destination.show',
+          params: { id: destination.id },
+        }"
+        :key="destination.id"
+      >
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" alt="destination.name" />
+      </router-link>
     </div>
+  </div>
 </template>
 
 <script>
-import sourceData from '@/data.json'
+import sourceData from "@/data.json";
 export default {
-    data(){
-        return {
-            destinations: sourceData.destinations
-        }
-    }
-
-}
+  data() {
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
