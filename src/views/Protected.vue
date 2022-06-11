45df44e440945c5a9615f784449682b546923fa2
olddunk <yegc22@gmail.com>
2022-06-11 17:54:57 +0800

    chap4.28 Composition API

--- src/views/Protected.vue
+++ src/views/Protected.vue
@@ -2,6 +2,9 @@
   <div>
     <h1>Hello {{ username }}</h1>
     <button class="btn" @click="logout">Logout</button>
+    <router-link :to="{ name: 'invoices' }">
+      <button class="btn">invoices</button>
+    </router-link>
   </div>
 </template>
 <script>
